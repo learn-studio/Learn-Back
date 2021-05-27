a92105d9a5a7e99738c692d17a0df3cd899da7d0
bury_l <bury_l@etna-alternance.net>
2021-05-27 16:47:46 +0200

    Base api

--- learn-studio-api/app.js
+++ learn-studio-api/app.js
@@ -1,39 +1,13 @@
-var express = require('express');
-var swaggerUi = require('swagger-ui-express');
-var swaggerJSDoc = require('swagger-jsdoc');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const port = process.env.PORT
+const userRouter = require('./routes/user')
+require('./db/db')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express()
 
-var app = express();
+app.use(express.json())
+app.use(userRouter)
 
-var swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-      title: 'Express API for JSONPlaceholder',
-      version: '1.0.0',
-    },
-  };
-  
-var options = {
-    swaggerDefinition,
-    // Paths to files containing OpenAPI definitions
-    apis: ['./routes/*.js'],
-  };
-  
-var swaggerSpec = swaggerJSDoc(options);
-
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
