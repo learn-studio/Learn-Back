2988ece25171ef63cb4879416ad74098aed3e5f8
Léo BURY <dev@liosfr.net>
2021-05-28 13:58:07 +0000

    Merged PR 27: Ajout de l'api users
    Ajout de MONGODB
    Ajout des routes users
    Ajout des schémas mongoose
    
    Related work items: #31

--- learn-studio-api/app.js
+++ learn-studio-api/app.js
@@ -1,39 +1,18 @@
-var express = require('express');
-var swaggerUi = require('swagger-ui-express');
-var swaggerJSDoc = require('swagger-jsdoc');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const port = process.env.PORT
+const userRouter = require('./routes/user')
+const indexRouter = require('./routes/index');
+require('./db/db')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express()
 
-var app = express();
-
-var swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-      title: 'Express API for JSONPlaceholder',
-      version: '1.0.0',
-    },
-  };
-  
-var options = {
-    swaggerDefinition,
-    // Paths to files containing OpenAPI definitions
-    apis: ['./routes/*.js'],
-  };
-  
-var swaggerSpec = swaggerJSDoc(options);
-
-
-app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(userRouter)
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-module.exports = app;
+
+app.listen(port, () => {
+    console.log(`Server running on port  ${port}`)
+})
